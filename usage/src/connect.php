535b761f8437df00d54cab789d8b2499a4d0cf55
James Brooking <jambroo@gmail.com>
2016-03-29 22:52:22 +0000

    Connected!

--- usage/src/connect.php
+++ usage/src/connect.php
@@ -1,27 +1,18 @@
 <?php
+
 require '/vendor/predis/predis/src/Autoloader.php';
 
 Predis\Autoloader::register();
 
-// since we connect to default setting localhost
-// and 6379 port there is no need for extra
-// configuration. If not then you can specify the
-// scheme, host and port to connect as an array
-// to the constructor.
 try {
-    $redis = new Predis\Client();
-/*
-    $redis = new PredisClient(array(
-        "scheme" => "tcp",
-        "host" => "127.0.0.1",
-        "port" => 6379));
-*/
+    $redis_addr = getenv("REDIS_PORT");
+    $redis = new Predis\Client($redis_addr);
     echo "Successfully connected to Redis";
-}
-catch (Exception $e) {
+} catch (Exception $e) {
     echo "Couldn't connected to Redis";
     echo $e->getMessage();
 }
+
 $redis->set("hello_world", "Hi from php!");
 $value = $redis->get("hello_world");
 var_dump($value);
