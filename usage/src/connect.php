8ebc9feaf30cd7579209cc1cb53f872fc1b3f9a5
James Brooking <jambroo@gmail.com>
2016-03-29 23:38:51 +0000

    Got files.

--- usage/src/connect.php
+++ usage/src/connect.php
@@ -4,18 +4,43 @@ require '/vendor/predis/predis/src/Autoloader.php';
 
 Predis\Autoloader::register();
 
-try {
-    $redis_addr = getenv("REDIS_PORT");
-    $redis = new Predis\Client($redis_addr);
-    echo "Successfully connected to Redis";
-} catch (Exception $e) {
-    echo "Couldn't connected to Redis";
-    echo $e->getMessage();
+define("REDIS_PORT", getenv("REDIS_PORT"));
+define("BEFORE_DIR", "/opt/yo/sample/before");
+define("AFTER_DIR", "/opt/yo/sample/after");
+
+class FileEntries extends Predis\Client {
+    private $i;
+    private $files;
+
+    function __construct($addr) {
+       $this->i = 0;
+       $this->files = [];
+       parent::__construct($addr);
+    }
+
+    function crawl($dir) {
+        $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+        foreach($objects as $name => $object){
+            if ($object->getFilename() != '.' && $object->getFilename() != '..') {
+		$this->files[$this->i] = $name;
+	        $this->i++;
+            }
+        }
+    }
+
+    function output() {
+        print_r($this->files);
+    }
 }
 
-$redis->set("hello_world", "Hi from php!");
+/*
+print_r($redis->set("hello_world", "Hi from php!"));
 $value = $redis->get("hello_world");
 var_dump($value);
-
 echo ($redis->exists("Santa Claus")) ? "true" : "false";
+*/
 
+$fe = new FileEntries(REDIS_PORT);
+$fe->crawl(BEFORE_DIR);
+$fe->crawl(AFTER_DIR);
+$fe->output();
