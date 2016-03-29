f1b0b15559d11eb8b0ce6892ecfed85da67c5621
James Brooking <jambroo@gmail.com>
2016-03-29 22:45:30 +0000

    Progress.

---
+++ redis/stop.sh
@@ -0,0 +1,1 @@
+sudo docker stop fastlookup && sudo docker rm fastlookup
---
+++ usage/go.sh
@@ -0,0 +1,1 @@
+docker run --link fastlookup:redis -v /home/james/fastlookup/usage/src:/opt/yo -i -t php_composer /usr/local/bin/php /opt/yo/connect.php
